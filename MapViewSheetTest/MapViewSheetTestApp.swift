a5e3acbd79da44fbbd49a0420a70749dd3beeac9
Jacob01984 <Jacoblavenant@yahoo.com>
2023-07-29 17:45:54 -0500

    Initial Commit

---
+++ MapViewSheetTest/MapViewSheetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewSheetTestApp.swift
+//  MapViewSheetTest
+//
+//  Created by Jacob Lavenant on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MapViewSheetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
